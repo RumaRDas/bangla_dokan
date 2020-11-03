aac65fdbfefd73b4e3c022d6c1efe046b9293459
Ruma Das <rumaadl16@gmail.com>
2020-11-03 23:31:04 +1030

    adding name and email data field

--- models/user.js
+++ models/user.js
@@ -8,6 +8,12 @@ const userSchema = new mongoose.Schema({
         trim: true,
         required: true,
         maxlength: 32
-    }
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique:true
+    },
 
 })
