21696644e644b6fb7d53eaec1026f14f814149ed
Ruma Das <rumaadl16@gmail.com>
2020-11-03 22:50:33 +1030

    adding database connection in app.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+//app
+const app = express();
+
+//db
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(()=> console.log("Database connected"));
+
+//routes
 app.get('/', (req,res) =>{
     res.send("hello from node update");
 });
