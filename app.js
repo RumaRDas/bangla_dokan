e9561d8265a00e53e1c4d239bf6e460562111383
Ruma Das <rumaadl16@gmail.com>
2020-11-15 23:46:32 +1030

    adding get userById in controllers and routes

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const expressValidator = require('express-validator');
-require('dotenv').config();
+const expressValidator = require("express-validator");
+require("dotenv").config();
 
 //import routes
-const authRoutes = require('./routes/auth')
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
 //app
 const app = express();
 
 //db
-mongoose.connect(process.env.DATABASE,{
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useCreateIndex: true
-}).then(()=> console.log("Database connected"));
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database connected"));
 
 //middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 
 //routes mddleware
-app.use("/api",authRoutes);
-
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 const port = process.env.PORT || 8000;
 
-app.listen(port,() =>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const {userById}= require('../controllers/user.js');
+const { requireSignin } = require("../controllers/auth.js");
+const { userById } = require("../controllers/user");
+
+router.get("/secret/:userId", requireSignin, (req, res) => {
+  res.json({
+    user: req.profile,
+  });
+});
 
 router.param("userId", userById);
+
 module.exports = router;
