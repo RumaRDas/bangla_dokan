8d5d1d2b7bcce8cc7077c6ba717b30c75b92f190
Ruma Das <rumaadl16@gmail.com>
2020-11-17 12:43:51 +1030

    adding middle ware in product routs

--- routes/product.js
+++ routes/product.js
@@ -2,4 +2,13 @@ const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
 
+const { create } = require("../controllers/product");
+
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth.js");
+const { userById } = require("../controllers/user");
+
+router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+
+router.param("userId", userById);
+
 module.exports = router;
