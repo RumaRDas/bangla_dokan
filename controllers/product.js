f9c964811958b0ae125ba4060ba1c05687b4e9e9
Ruma Das <rumaadl16@gmail.com>
2020-11-17 12:10:14 +1030

    aadding product models , controllers, router and adding product router in app.js

--- controllers/product.js
+++ controllers/product.js
@@ -1,17 +1,3 @@
-const mongoose = require("mongoose");
-const crypto = require("crypto");
-const { v1: uuidv1 } = require("uuid");
+const Product = require("../controllers/product");
 
-const productSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-      maxlength: 32,
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Product", productSchema);
+exports.create = (req, res) => {};
