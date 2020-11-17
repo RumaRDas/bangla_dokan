9e3dd0d48285d650b4505749216ae4cf06ad45d8
Ruma Das <rumaadl16@gmail.com>
2020-11-18 01:15:06 +1030

    adding find category list

--- controllers/category.js
+++ controllers/category.js
@@ -55,4 +55,13 @@ exports.remove = (req, res) => {
     });
   });
 };
-exports.list = (req, res) => {};
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        errror: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
