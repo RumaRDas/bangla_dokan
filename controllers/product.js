75c8f4acba3bfb1ba3a41f8c5f6502ee3cb8fd4a
Ruma Das <rumaadl16@gmail.com>
2020-11-17 13:11:03 +1030

    adding product controller for saving product in database

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,37 @@
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 const Product = require("../controllers/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  // req form data
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
+
+    let product = new Product(fields);
+
+    //uploading photo data and file type in database
+    if (files.photo) {
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    // if No error save product to database
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json({ result });
+    });
+  });
+};
