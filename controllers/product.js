44a4f74bc6d84f9e2c181facd4578cb0684c7a8e
Ruma Das <rumaadl16@gmail.com>
2020-11-19 00:18:47 +1030

    adding find all products methods

--- controllers/product.js
+++ controllers/product.js
@@ -158,3 +158,23 @@ exports.update = (req, res) => {
  * by arrival = /products?sortBy=createdAt&order=desc&limit=6
  * if no params are sent, then all products are returned
  */
+
+exports.list = (req, res) => {
+  let order = req.query.order ? req.query.order : "asc";
+  let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+  let limit = req.query.limit ? req.query.limit : 6;
+
+  Product.find()
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, products) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Product not found",
+        });
+      }
+      res.json(products);
+    });
+};
