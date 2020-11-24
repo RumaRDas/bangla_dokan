b5bc9abe2796fcc3eab55a1f8085dbe2d80d8e59
Ruma Das <rumaadl16@gmail.com>
2020-11-24 23:33:12 +1030

    creating menu pages

---
+++ frontend/src/core/Menu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-primary">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
