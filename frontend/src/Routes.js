850d7b4e111937f3cbf8ae978c0535f818fb2f37
Ruma Das <rumaadl16@gmail.com>
2020-11-24 23:11:03 +1030

    adding routes in frontend and added routes file in index.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
 
 const Routes = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <switch>
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+      </switch>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
