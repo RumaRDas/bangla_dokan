d429a49d645dc532c571bf5ae28a2b4a1cfd3d4e
Ruma Das <rumaadl16@gmail.com>
2020-11-25 00:39:00 +1030

    creating a signup form

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,11 +1,36 @@
 import React from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
+
 const Signup = () => {
+  const signUpForm = () => {
+    return (
+      <form>
+        <div className="form-group">
+          <label className="text-muted">Name</label>
+          <input typr="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Email</label>
+          <input typr="email" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Password</label>
+          <input typr="password" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    );
+  };
+
   return (
     <Layout
       title="SignUp Page"
       description="Signup to Node React E-commerce App"
-    ></Layout>
+      className="container col-md-8 offset-md-2"
+    >
+      {signUpForm()}
+    </Layout>
   );
 };
 
