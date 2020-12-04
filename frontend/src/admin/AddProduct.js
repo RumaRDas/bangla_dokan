a825627fa12dbdd9ee5f5b454762ee3bcc9f1e02
Ruma Das <rumaadl16@gmail.com>
2020-12-04 21:56:19 +1030

    adding admin product add page with photo upload

--- frontend/src/admin/AddProduct.js
+++ frontend/src/admin/AddProduct.js
@@ -6,6 +6,48 @@ import { createProduct } from "./apiAdmin";
 
 const AddProduct = () => {
   const { user, token } = isAuthenticated();
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "",
+    categories: [],
+    category: "",
+    shipping: "",
+    quantity: "",
+    photo: "",
+    loading: false,
+    error: "",
+    createProduct: "",
+    redirectToProfile: false,
+    formData: "",
+  });
+
+  const {
+    name,
+    description,
+    price,
+    categories,
+    category,
+    shipping,
+    quantity,
+    loading,
+    error,
+    createProduct,
+    redirectToProfile,
+    formData,
+  } = values;
+
+  const newPostForm = () => {
+    return (
+      <form className="mb-3">
+        <h4>Post Photo</h4>
+        <div className="form-group">
+          <label></label>
+          <input type="file" name="poto" />
+        </div>
+      </form>
+    );
+  };
 
   return (
     <Layout
@@ -13,7 +55,7 @@ const AddProduct = () => {
       description={`G'Day ${user.name}, ready to add a new Product`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">.....</div>
+        <div className="col-md-8 offset-md-2">{newPostForm()}</div>
       </div>
     </Layout>
   );
