330b614580a36bdda0835ca58ddff746245bf9ea
Ruma Das <rumaadl16@gmail.com>
2020-12-06 21:05:48 +1030

    adding find product by sold and created

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,35 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import { getProducts } from "./apicor";
 
 const Home = () => {
-  const [productBySell, setProductBySell] = useState([]);
-  const [peoductByArrival, setProductByArrival] = useState([]);
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
   const [error, setError] = useState(false);
 
-  const LoadProductBySell = () => {
+  //finding products by sold number
+  const loadProductsBySell = () => {
     getProducts("sold").then((data) => {
       if (data.error) {
         setError(data.Error);
       } else {
-        setProductBySell(data);
+        setProductsBySell(data);
       }
     });
   };
 
-  const LoadProductByArrival = () => {
+  //Finding products by new arrival
+  const loadProductsByArrival = () => {
     getProducts("createdAt").then((data) => {
       if (data.error) {
         setError(data.Error);
       } else {
-        setProductByArrival(data);
+        setProductsByArrival(data);
       }
     });
   };
 
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
   return (
     <Layout title="Home Page" description="Node React E-commerce App">
-      <h2>Home Of Bangladokan</h2>
+      {JSON.stringify(productsBySell)}
+      <hr />
+      {JSON.stringify(productsByArrival)}
     </Layout>
   );
 };
