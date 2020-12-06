54eba23e34e5ad12a2e2090a5800fefbca26a8f8
Ruma Das <rumaadl16@gmail.com>
2020-12-06 20:47:23 +1030

    adding sell and arrival product find method

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -7,6 +7,26 @@ const Home = () => {
   const [peoductByArrival, setProductByArrival] = useState([]);
   const [error, setError] = useState(false);
 
+  const LoadProductBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.Error);
+      } else {
+        setProductBySell(data);
+      }
+    });
+  };
+
+  const LoadProductByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      if (data.error) {
+        setError(data.Error);
+      } else {
+        setProductByArrival(data);
+      }
+    });
+  };
+
   return (
     <Layout title="Home Page" description="Node React E-commerce App">
       <h2>Home Of Bangladokan</h2>
