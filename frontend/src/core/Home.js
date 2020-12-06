f0ccc925caca627e7673155f60f7d21ac33a4759
Ruma Das <rumaadl16@gmail.com>
2020-12-06 20:41:07 +1030

    decaring use state

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apicor";
 
 const Home = () => {
+  const [productBySell, setProductBySell] = useState([]);
+  const [peoductByArrival, setProductByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
   return (
     <Layout title="Home Page" description="Node React E-commerce App">
       <h2>Home Of Bangladokan</h2>
