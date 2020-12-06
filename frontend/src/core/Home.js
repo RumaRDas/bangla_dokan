104d97410f3593d2cb2da0874ebe33a5dd93f1fc
Ruma Das <rumaadl16@gmail.com>
2020-12-06 22:14:13 +1030

    adding component for showing image for products

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -37,7 +37,11 @@ const Home = () => {
   }, []);
 
   return (
-    <Layout title="Home Page" description="Node React E-commerce App">
+    <Layout
+      title="Home Page"
+      description="Node React E-commerce App"
+      className="container-fluid"
+    >
       <h2 className="mb-4">New Arrivals</h2>
       <div className="row">
         {productsByArrival.map((product, i) => {
