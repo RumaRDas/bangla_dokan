88f685c6ef0c685bb62a17492f3d0e4853745249
Ruma Das <rumaadl16@gmail.com>
2020-12-08 18:36:01 +1030

    adding get single product api and method for getting product

--- frontend/src/core/Product.js
+++ frontend/src/core/Product.js
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { getProducts } from "./apicor";
+import { read } from "./apicor";
 import Card from "./Card";
 
-const Product = () => {
+const Product = (props) => {
+  const [product, setProduct] = useState({});
+  const [error, setError] = useState(false);
+
+  const loadSingleProduct = (productId) => {
+    read(productId).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProduct(data);
+      }
+    });
+  };
+  useEffect(() => {
+    const productId = props.match.params.productId;
+    loadSingleProduct(productId);
+  }, []);
+
   return (
     <Layout
       title="Home Page"
       description="Node React E-commerce App"
       className="container-fluid"
     >
-      <h2>Product page</h2>
+      <h2 className="mb-4">Single Product</h2>
+      <div className="row">{JSON.stringify(product)}</div>
     </Layout>
   );
 };
