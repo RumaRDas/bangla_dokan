8b792e6230423bf5ccdcf08f47b94ab3c41bbb0e
Ruma Das <rumaadl16@gmail.com>
2020-12-09 22:33:12 +1030

    adding product in cart

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import ShowImage from "./ShowImage";
 import moment from "moment";
+import { addItem } from "./cartHelpers";
 
 const Card = ({ product, showViewProductButton = true }) => {
+  const [redirect, setRedirect] = useState(false);
   //substring(0,10) is function for showing limit text
 
   const showViewbutton = (showViewProductButton) => {
@@ -18,9 +20,22 @@ const Card = ({ product, showViewProductButton = true }) => {
     );
   };
 
+  const addToCart = () => {
+    addItem(product, setRedirect(true));
+  };
+
+  const shouldRedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
   const showAddToCartBtn = () => {
     return (
-      <button className="btn btn-outline-warning mt-2 mb-2 card-btn-1  ">
+      <button
+        onClick={addToCart}
+        className="btn btn-outline-warning mt-2 mb-2 card-btn-1  "
+      >
         Add to cart
       </button>
     );
@@ -38,6 +53,7 @@ const Card = ({ product, showViewProductButton = true }) => {
     <div className="card">
       <div className="card-header name"> {product.name} </div>
       <div className="card-body">
+        {shouldRedirect(redirect)}
         <ShowImage item={product} url="product" />
         <p className="lead mt-2">{product.description.substring(0, 100)}</p>
         <p className="black-10">${product.price}</p>
