884175fa64628b49369da99b9d00ff544de2c2bd
Ruma Das <rumaadl16@gmail.com>
2020-12-16 22:52:01 +1030

    adding checkout component

---
+++ frontend/src/core/Checkout.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import { getProducts } from "./apicor";
+import Card from "./Card";
+import Search from "./Search";
+
+const Checkout = () => {
+  return <div></div>;
+};
+
+export default Checkout;
