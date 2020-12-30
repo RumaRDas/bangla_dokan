90459bd2087117e6daff62257832792e1feaf23d
Ruma Das <rumaadl16@gmail.com>
2020-12-31 00:53:17 +1030

    adding middle ware for sold item products increment and decrement

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth.js");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { create } = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
   "/order/create/:userId",
   requireSignin,
   isAuth,
   addOrderToUserHistory,
+  decreaseQuantity,
   create
 );
 router.param("userId", userById);
