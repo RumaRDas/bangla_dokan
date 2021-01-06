bdb68322cd2fb87e959b1345d13bdbb1bd9c41f2
Ruma Das <rumaadl16@gmail.com>
2021-01-06 22:13:55 +1030

    adding a component for usr profile update

---
+++ frontend/src/user/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Profile</h2>
+    </div>
+  );
+};
+
+export default Profile;
