ef4acac78ef818f8a35cd2b7f03cc0ba52775f8b
Ruma Das <rumaadl16@gmail.com>
2021-01-06 23:12:24 +1030

    adding method for getting user informaton in profile update

--- frontend/src/user/Profile.js
+++ frontend/src/user/Profile.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { read, update, updateUser } from "./apiUser";
 
-const Profile = () => {
+const Profile = ({ match }) => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: false,
+    success: false,
+  });
+
+  const { token } = isAuthenticated();
+  const { name, email, password, success, error } = values;
+
+  const init = (userId) => {
+    // console.log(userId);
+    read(userId, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({
+          ...values,
+          name: data.name,
+          email: data.email,
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(match.params.userId);
+  }, []);
+
   return (
-    <div>
-      <h2>Profile</h2>
-    </div>
+    <Layout
+      title="Profile"
+      description="Update your Profile"
+      className="container-fluid"
+    >
+      <h2 className="mb-4">Profile Update</h2>
+      {JSON.stringify(values)}
+    </Layout>
   );
 };
 
