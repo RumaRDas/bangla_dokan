fdbe72431c79c5985f8bc4c72f359ded38bf13b6
Ruma Das <rumaadl16@gmail.com>
2021-01-08 00:09:13 +1030

    adding purchase history to user dashboard

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,6 @@
+const { errorHandler } = require("../helpers/dbErrorHandler");
 const User = require("../models/user");
+const { Order } = require("../models/order");
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -18,22 +20,64 @@ exports.read = (req, res) => {
   return res.json(req.profile);
 };
 
+// exports.update = (req, res) => {
+//   User.findByIdAndUpdate(
+//     { _id: req.profile._id },
+//     { $set: req.body },
+//     { new: true },
+//     (err, user) => {
+//       if (err) {
+//         return res.status(400).json({
+//           erroe: "You are not authorized to perform this action",
+//         });
+//       }
+//       user.hashed_passwor = undefined;
+//       user.salt = undefined;
+//       res.json(user);
+//     }
+//   );
+// };
+
 exports.update = (req, res) => {
-  User.findByIdAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true },
-    (err, user) => {
-      if (err) {
+  // console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body);
+  const { name, password } = req.body;
+
+  User.findOne({ _id: req.profile._id }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+    if (!name) {
+      return res.status(400).json({
+        error: "Name is required",
+      });
+    } else {
+      user.name = name;
+    }
+
+    if (password) {
+      if (password.length < 6) {
         return res.status(400).json({
-          erroe: "You are not authorized to perform this action",
+          error: "Password should be min 6 characters long",
+        });
+      } else {
+        user.password = password;
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log("USER UPDATE ERROR", err);
+        return res.status(400).json({
+          error: "User update failed",
         });
       }
-      user.hashed_passwor = undefined;
-      user.salt = undefined;
-      res.json(user);
-    }
-  );
+      updatedUser.hashed_password = undefined;
+      updatedUser.salt = undefined;
+      res.json(updatedUser);
+    });
+  });
 };
 
 exports.addOrderToUserHistory = (req, res, next) => {
@@ -64,4 +108,16 @@ exports.addOrderToUserHistory = (req, res, next) => {
       next();
     }
   );
+};
+
+exports.purchaseHistory = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name")
+    .sort("-created")
+    .exec((err, orders) => {
+      if (err) {
+        return res.status(400).json({ error: errorHandler(err) });
+      }
+      res.json(orders);
+    });
 };
