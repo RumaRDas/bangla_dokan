77f582939d5ed36b975ccad04872f50f39418ed3
Ruma Das <rumaadl16@gmail.com>
2021-01-13 00:27:46 +1030

    get product method for getting a single products

--- frontend/src/admin/apiAdmin.js
+++ frontend/src/admin/apiAdmin.js
@@ -126,3 +126,13 @@ export const deleteProduct = (productId, userId, token) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const getProduct = (productId) => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
